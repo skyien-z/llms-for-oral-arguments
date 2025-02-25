c72012635c0bd6d12f5783f09bb6a4cd64f0b645
Kylie Zhang <kz9921@della-gpu.princeton.edu>
2025-02-25 15:17:12 -0500

    Add valence assessments for new questions

---
+++ kylie_os_qgen/script_create_questions_qid_csvs.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import hashlib
+
+io_dir = "../datasets/llm_outputs/generate_questions/"
+all_questions_csv = io_dir + "2024_full_text_all_justices_questions_llama70b.csv"
+
+df = pd.read_csv(all_questions_csv)
+df.drop(["Unnamed: 0"], axis=1, inplace=True)
+df.rename(columns={"petitioner_opening_text": "petitioner_opening_statement"}, inplace=True)
+
+def get_question_id(transcript_id, question_text):
+    unique_string = f"{transcript_id}_{question_text}"
+    id_hash = hashlib.md5(unique_string.encode()).hexdigest()[:8] # use first 8 chars
+    return f'q_{id_hash}'
+
+current_justices = {"roberts", "thomas", "alito", "sotomayor", "kagan", "gorsuch", "kavanaugh", "barrett", "jackson"}
+question_addressees = ["petitioner", "respondent"]
+
+all_questions_df = pd.DataFrame()
+for justice in current_justices:
+    for addressee in question_addressees:
+        for _, row in df.iterrows():
+            # get list of questions from a justice toward addressee for transcript
+            generated_questions = eval(row[f"questions_{justice}_{addressee}"])
+            for question in generated_questions:
+                new_question_row_df = pd.DataFrame({
+                                                "question_id": [get_question_id(row["transcript_id"], question)], 
+                                                "transcript_id": row["transcript_id"],
+                                                "question_addressee": addressee,
+                                                "justice": justice,
+                                                "opening_statement": row[f"{addressee}_opening_statement"],
+                                                "question_text": question,
+                                                "full_text": row[f"{addressee}_full_text"]
+                                                })
+                all_questions_df = pd.concat([all_questions_df, new_question_row_df], ignore_index=True)
+
+all_questions_df.to_csv(io_dir+"2024_questions_all_justices_llama70b_with_qids.csv")
