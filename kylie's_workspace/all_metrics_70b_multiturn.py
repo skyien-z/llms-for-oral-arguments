094539488d780cfb52c85822cb436fb9ee5ee033
Kylie Zhang <kz9921@della-gpu.princeton.edu>
2025-04-08 21:18:46 -0400

    Add latest vllm metrics classifications

--- kylie's_workspace/all_metrics_70b_multiturn.py
+++ kylie's_workspace/all_metrics_70b_multiturn.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import os
 from vllm import LLM
 from transformers import AutoTokenizer
+import argparse
 from multiturn_classification_utils import classify_questions_valence, classify_questions_legalbench, classify_questions_metacog, classify_questions_realness, classify_questions_helpfulness, classify_questions_similarity, classify_questions_preference, classify_questions_overall
 
 # Find file in git repo
@@ -21,40 +22,40 @@ model_id = '/scratch/gpfs/kz9921/transformer_cache/Llama-3.3-70B-Instruct'
 llm = LLM(model=model_id, tensor_parallel_size=4)
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-############################################################################
-## Update Input File Path!
-input_file = './test.jsonl'
-############################################################################
-all_questions_df = pd.read_json(get_file_path(input_file), lines=True)  # read in jsonl
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_jsonl_path', help="Need an input files on which to run script!")
+    parser.add_argument('output_jsonl_path', help="Need an output path to save your metrics!")
+    parser.add_argument('num_samples', type=int, help='Number of samples to test')
+    args = parser.parse_args()
 
-# iterate through all json lines
-for index, row in all_questions_df.iterrows():
-    context = row['context']
-    predictions = row["predictions"]
-    actual_text = row["text"]
+    all_questions_df = pd.read_json(get_file_path(args.input_jsonl_path), lines=True)  # read in jsonl
+    all_questions_df[:300]
+    # iterate through all json lines
+    for _, row in all_questions_df.iterrows():
+        context = row['context']
+        predictions = row["predictions"]
+        actual_text = row["text"]
 
-    # for which justices on whom we ran inference, classify the generated questions
-    for justice in predictions.keys():
-        justice_metrics = predictions[justice]
-        generated_text = justice_metrics["model_response"]
+        # for which justices on whom we ran inference, classify the generated questions
+        for justice in predictions.keys():
+            justice_metrics = predictions[justice]
+            generated_text = justice_metrics["model_response"]
 
-        # run classifications
-        justice_metrics["valence"] = classify_questions_valence(llm, tokenizer, context, justice, generated_text)
-        justice_metrics["legalbench"] = classify_questions_legalbench(llm, tokenizer, context, justice, generated_text)
-        justice_metrics["metacog"] = classify_questions_metacog(llm, tokenizer, context, justice, generated_text)
-        justice_metrics["how_overall"] = classify_questions_overall(llm, tokenizer, context, justice, generated_text)
-        justice_metrics["is_realistic"] = classify_questions_realness(llm, tokenizer, context, justice, generated_text)
-        justice_metrics["how_helpful"] = classify_questions_helpfulness(llm, tokenizer, context, justice, generated_text)
-        justice_metrics["how_similar"] = classify_questions_similarity(llm, tokenizer, context, justice, generated_text, actual_text)
-        justice_metrics["preference"] = classify_questions_preference(llm, tokenizer, context, justice, generated_text, actual_text)
+            # run classifications
+            justice_metrics["valence"] = classify_questions_valence(llm, tokenizer, context, justice, generated_text)
+            justice_metrics["legalbench"] = classify_questions_legalbench(llm, tokenizer, context, justice, generated_text)
+            justice_metrics["metacog"] = classify_questions_metacog(llm, tokenizer, context, justice, generated_text)
+            justice_metrics["how_overall"] = classify_questions_overall(llm, tokenizer, context, justice, generated_text)
+            justice_metrics["is_realistic"] = classify_questions_realness(llm, tokenizer, context, justice, generated_text)
+            justice_metrics["how_helpful"] = classify_questions_helpfulness(llm, tokenizer, context, justice, generated_text)
+            justice_metrics["how_similar"] = classify_questions_similarity(llm, tokenizer, context, justice, generated_text, actual_text)
+            justice_metrics["preference"] = classify_questions_preference(llm, tokenizer, context, justice, generated_text, actual_text)
 
-############################################################################
-## Update Output File Path!
-output_dir = 'generated_data/'
-output_file = 'sanity_check_v3.csv'
-############################################################################
+    # Save to a .jsonl file
+    jsonl_string = all_questions_df.to_json(orient='records', lines=True)
+    with open(args.output_jsonl_path, 'w') as f:
+        f.write(jsonl_string)
 
-jsonl_string = all_questions_df.to_json(orient='records', lines=True)
-# Save to a .jsonl file
-with open(output_dir + output_file, 'w') as f:
-    f.write(jsonl_string)
+if __name__ == "__main__":
+    main()
